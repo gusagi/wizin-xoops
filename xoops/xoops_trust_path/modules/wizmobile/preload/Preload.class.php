eb8b81acd96f372c7ceaf137bb48725b47840e16
gusagi <gusagi@gmail.com>
2008-05-02 17:16:52 +0000

    commit before remove file.
    git-svn-id: file://localhost/home/repos/svn/wizin/trunk@49 fd31610b-4be5-dc11-a523-00018064d71f

--- xoops/xoops_trust_path/modules/wizmobile/preload/Preload.class.php
+++ xoops/xoops_trust_path/modules/wizmobile/preload/Preload.class.php
@@ -2,47 +2,100 @@
 /**
  * WizMobile module preload script for XOOPS Cube Legacy2.1
  *
- * PHP Versions 4
+ * PHP Versions 4.4.X or upper version
  *
  * @package  WizMobile
- * @author  gusagi<gusagi@gusagi.com>
- * @copyright  2007 - 2008 gusagi
+ * @author  Makoto Hashiguchi a.k.a. gusagi<gusagi@gusagi.com>
+ * @copyright 2008 Makoto Hashiguchi
+ * @license GNU General Public License Version2
+ *
+ */
+
+/**
+ * GNU General Public License Version2
+ *
+ * Copyright (C) 2008  < Makoto Hashiguchi a.k.a. gusagi >
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
 
 if( ! defined( 'XOOPS_ROOT_PATH' ) ) exit ;
+
+$initScript = dirname( dirname(__FILE__) ) . '/init.php';
+if ( file_exists($initScript) && is_readable($initScript) ) {
+    require_once $initScript;
+}
 
 if ( ! class_exists('CallWizMobile') ) {
     class CallWizMobile extends XCube_ActionFilter
     {
         function preBlockFilter()
         {
-            $initScript = dirname( dirname(__FILE__) ) . '/init.php';
-            if ( file_exists($initScript) && is_readable($initScript) ) {
-                require_once $initScript;
+            $wizMobile =& WizMobile::getSingleton();
+            preg_match( "/(\w+)\.class\.php/", strtolower(basename(__FILE__)), $matches );
+            $frontDirname = str_replace( '_' . $matches[1], '', get_class($this) );
+            $actionScript = dirname( dirname(__FILE__) ) . '/class/WizMobile_Action.class.php';
+            if ( file_exists($actionScript) ) {
+                require_once $actionScript;
+                if ( class_exists($className) ) {
+                    $wizMobileAction = new $className();
+                    $wizMobile->setActionClass( $wizMobileAction );
+                }
             }
             parent::preBlockFilter();
         }
 
         function postFilter()
         {
+            // test code >>
+            if ( strpos($_SERVER['REQUEST_URI'], 'PHPSESSID') !== false ) {
+                $urlArray = explode( 'PHPSESSID', WIZXC_CURRENT_URI );
+                $url = $urlArray[0];
+                if ( substr($url, -1, 1) === '?' || substr($url, -1, 1) === '&' ) {
+                    $url = substr( $url, 0, strlen($url) - 1 );
+                }
+                header( "HTTP/1.1 404 Not Found" );
+                exit();
+            }
+            // test code <<
+            $wizMobile =& WizMobile::getSingleton();
             $user = & Wizin_User::getSingleton();
             if ( $user->bIsMobile ) {
                 $xcRoot =& XCube_Root::getSingleton();
                 // exchange theme
-                $wizMobile =& WizMobile::getSingleton();
                 $wizMobile->exchangeTheme();
                 // regenerate session id
-                $xcRoot->mDelegateManager->add( 'Site.CheckLogin.Success', array($wizMobile, 'directLoginSuccess') );
+                $xcRoot->mDelegateManager->add( 'Site.CheckLogin.Success', array($wizMobile, 'directLoginSuccess'), XCUBE_DELEGATE_PRIORITY_FINAL );
                 $xcRoot->mDelegateManager->add( 'Site.CheckLogin.Fail', array($wizMobile, 'directLoginFail') );
-                $xcRoot->mDelegateManager->add( 'Site.Logout.Success', array($wizMobile, 'directLogout'), XCUBE_DELEGATE_PRIORITY_FINAL+1 );
-                $xcRoot->mDelegateManager->add( 'Site.Logout.Fail', array($wizMobile, 'directLogout'), XCUBE_DELEGATE_PRIORITY_FINAL+1 );
-                $xcRoot->mDelegateManager->add( 'Legacy_AdminControllerStrategy.SetupBlock', array($wizMobile, 'denyAccessAdminArea'), XCUBE_DELEGATE_PRIORITY_FIRST );
-                // check session
-                $wizMobile->checkMobileSession();
+                $xcRoot->mDelegateManager->add( 'Site.Logout.Success', array($wizMobile, 'directLogout'),
+                    XCUBE_DELEGATE_PRIORITY_FINAL+1 );
+                $xcRoot->mDelegateManager->add( 'Site.Logout.Fail', array($wizMobile, 'directLogout'),
+                    XCUBE_DELEGATE_PRIORITY_FINAL+1 );
+                $xcRoot->mDelegateManager->add( 'Legacy_AdminControllerStrategy.SetupBlock',
+                    array($wizMobile, 'denyAccessAdminArea'), XCUBE_DELEGATE_PRIORITY_FIRST );
                 // insert session_id
-                ob_start( array($wizMobile, '_obTransSid') );
-                ob_start( array($wizMobile, '_obDirectRedirect') );
+                if ( ! $user->bIsBot ) {
+                    // check session
+                    $wizMobile->checkMobileSession();
+                } else {
+                    $wizMobile->checkSessionFixation();
+                }
+            } else {
+                $wizMobile->checkSessionFixation();
             }
             parent::postFilter();
         }
@@ -50,8 +103,8 @@ if ( ! class_exists('CallWizMobile') ) {
 }
 
 $mod_dir = basename( dirname(dirname($frontFile)) );
-preg_match( "/(\w+)\.class\.php/", basename($frontFile), $matches );
-$className = ucfirst($mod_dir) . "_" . $matches[1];
+preg_match( "/(\w+)\.class\.php/", strtolower(basename(__FILE__)), $matches );
+$className = $mod_dir . "_" . $matches[1];
 if ( ! class_exists($className) ) {
     eval( "class $className extends CallWizMobile {}" );
 }
