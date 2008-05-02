6f8168919ca5d82a76c6787b093de429f191efda
gusagi <gusagi@gmail.com>
2008-05-02 15:53:45 +0000

    commit before rename files.
    git-svn-id: file://localhost/home/repos/svn/wizin/trunk@48 fd31610b-4be5-dc11-a523-00018064d71f

--- xoops/xoops_trust_path/modules/wizxc/class/WizXcUtil.class.php
+++ xoops/xoops_trust_path/modules/wizxc/class/WizXcUtil.class.php
@@ -4,13 +4,39 @@
  * PHP Versions 4
  *
  * @package  WizXc
- * @author  gusagi<gusagi@gusagi.com>
- * @copyright  2007 - 2008 gusagi
+ * @author  Makoto Hashiguchi a.k.a. gusagi<gusagi@gusagi.com>
+ * @copyright 2008 Makoto Hashiguchi
+ * @license GNU General Public License Version2
  *
  */
 
-if ( ! class_exists('WizXcUtil') ) {
-    class WizXcUtil
+/**
+ * GNU General Public License Version2
+ *
+ * Copyright (C) 2008  < Makoto Hashiguchi a.k.a. gusagi >
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+if ( ! class_exists('WizXc_Util') ) {
+    require_once XOOPS_ROOT_PATH."/class/xoopsblock.php";
+    require_once XOOPS_ROOT_PATH."/class/template.php";
+
+    class WizXc_Util
     {
         function sessionDestroy()
         {
@@ -18,6 +44,61 @@ if ( ! class_exists('WizXcUtil') ) {
             $xcRoot->mSession->destroy();
             $xcRoot->mSession->start();
             $_SESSION = array();
+        }
+
+        function installD3Templates( $module, &$log, $templatesDir )
+        {
+            $tplHandler =& xoops_gethandler('tplfile');
+            $mid = $module->getVar( 'mid' );
+            $dirname = $module->getVar( 'dirname' );
+
+            if ( $handler = @ opendir($templatesDir . '/') ) {
+                while ( ($fileName = readdir($handler)) !== false ) {
+                    if ( strcmp(substr($fileName , 0 , 1), '.') === 0 ) {
+                        continue ;
+                    }
+                    $filePath = $templatesDir . '/' . $fileName ;
+                    if ( is_file($filePath) && is_readable($filePath) ) {
+
+                        //
+                        // Create template file object, then store it.
+                        //
+                        $tplfile =& $tplHandler->create();
+                        $tplfile->setVar( 'tpl_refid', $mid );
+                        $tplfile->setVar( 'tpl_lastimported', 0 );
+                        $tplfile->setVar( 'tpl_lastmodified', time() );
+
+                        if ( preg_match("/\.css$/i", $fileName) ) {
+                            $tplfile->setVar( 'tpl_type', 'css' );
+                        } else {
+                            $tplfile->setVar( 'tpl_type', 'module' );
+                        }
+
+                        $source = file( $filePath );
+                        $source = implode( '', $source );
+                        $tplfile->setVar( 'tpl_source', $source, true );
+                        $tplfile->setVar( 'tpl_module', $dirname );
+                        $tplfile->setVar( 'tpl_tplset', 'default' );
+                        $tplFileName = $dirname . '_' . $fileName;
+                        $tplfile->setVar( 'tpl_file', $tplFileName, true );
+                        $tplfile->setVar( 'tpl_desc', '', true );
+
+                        if ( $tplHandler->insert($tplfile) ) {
+                            $log->addReport( XCube_Utils::formatMessage(_AD_LEGACY_MESSAGE_TEMPLATE_INSTALLED, $fileName) );
+                        } else {
+                            $log->addError( XCube_Utils::formatMessage(_AD_LEGACY_ERROR_COULD_NOT_INSTALL_TEMPLATE, $fileName) );
+                            return false;
+                        }
+                    }
+                }
+            }
+            xoops_template_clear_module_cache( $mid );
+        }
+
+        function getXoopsTpl()
+        {
+            $xoopsTpl = new XoopsTpl();
+            return $xoopsTpl;
         }
     }
 }
