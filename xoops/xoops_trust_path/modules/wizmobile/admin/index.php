05703173078e8c7e015a8c5d930c57cdfa1736ce
gusagi <gusagi@gmail.com>
2008-05-04 01:21:21 +0000

    commit before rename files.
    git-svn-id: file://localhost/home/repos/svn/wizin/trunk@51 fd31610b-4be5-dc11-a523-00018064d71f

--- xoops/xoops_trust_path/modules/wizmobile/admin/index.php
+++ xoops/xoops_trust_path/modules/wizmobile/admin/index.php
@@ -45,12 +45,41 @@ $xoopsTpl = WizXc_Util::getXoopsTpl();
 $frontDirname = str_replace( '_wizmobile_action', '', strtolower(get_class($this)) );
 $tplFile = 'db:' . $frontDirname . '_admin_index.html';
 
-Wizin_Util::define( 'AAA', $frontDirname, '12345' );
+//
+// system status
+//
+$systemStatus = array();
+
+// image resize
+$createDir = XOOPS_ROOT_PATH . '/uploads/wizmobile';
+if ( extension_loaded('gd') && file_exists($createDir) && is_dir($createDir) && is_writable($createDir) ) {
+    $systemStatus['imageResize']['result'] = Wizin_Util::constant( 'WIZMOBILE_ENABLE' );
+} else {
+    $systemStatus['imageResize']['result'] = Wizin_Util::constant( 'WIZMOBILE_DISABLE' );
+    $systemStatus['imageResize']['messages'][] = Wizin_Util::constant( 'WIZMOBILE_MSG_GD_NOT_EXISTS' );
+}
+
+// partition page
+if ( class_exists('DOMDocument') && class_exists('SimpleXMLElement') ) {
+    $systemStatus['partitionPage']['result'] = Wizin_Util::constant( 'WIZMOBILE_ENABLE' );
+    if ( ! function_exists('tidy_repair_string') ) {
+        $systemStatus['partitionPage']['messages'][] = Wizin_Util::constant( 'WIZMOBILE_MSG_TIDY_NOT_EXISTS' );
+    }
+} else {
+    $systemStatus['partitionPage']['result'] = Wizin_Util::constant( 'WIZMOBILE_DISABLE' );
+    if ( ! class_exists('DOMDocument') ) {
+        $systemStatus['partitionPage']['messages'][] = Wizin_Util::constant( 'WIZMOBILE_MSG_DOM_NOT_EXISTS' );
+    }
+    if ( ! class_exists('SimpleXMLElement') ) {
+        $systemStatus['partitionPage']['messages'][] = Wizin_Util::constant( 'WIZMOBILE_MSG_SIMPLEXML_NOT_EXISTS' );
+    }
+}
 
 // call header
 require_once XOOPS_ROOT_PATH . '/header.php';
 
 // display main templates
+$xoopsTpl->assign( 'systemStatus', $systemStatus );
 $xoopsTpl->display( $tplFile );
 
 // call footer
