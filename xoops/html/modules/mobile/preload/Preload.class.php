4961d275e1d007130e70570c300624cabc395ee3
gusagi <gusagi@gmail.com>
2008-06-15 16:51:31 +0000

    add dummy file for old preload script.
    git-svn-id: file://localhost/home/repos/svn/wizin/trunk@85 fd31610b-4be5-dc11-a523-00018064d71f

---
+++ xoops/html/modules/mobile/preload/Preload.class.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * dummy script for "WizMobile" module series on XOOPS Cube Legacy2.1
+ *
+ * PHP Versions 4
+ *
+ * @package  WizMobile
+ * @author  gusagi<gusagi@gusagi.com>
+ * @copyright  2007 - 2008 gusagi
+ *
+ */
