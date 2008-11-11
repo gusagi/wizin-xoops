1ee58e195d81231f3bc0923e4a8205ba20a2b9c7
gusagi <gusagi@gmail.com>
2008-11-11 02:45:21 +0000

    Modified framework core files.
    git-svn-id: file://localhost/home/repos/svn/wizin/trunk@184 fd31610b-4be5-dc11-a523-00018064d71f

---
+++ wizin/src/core/App.class.php
@@ -0,0 +1,220 @@
+<?php
+/**
+ * Wizin framework core application class
+ *
+ * PHP Version 5.2 or Upper version
+ *
+ * @package  giftbox.in
+ * @author  Makoto Hashiguchi a.k.a. gusagi<gusagi@gusagi.com>
+ * @copyright 2008 Makoto Hashiguchi
+ * @license http://www.opensource.org/licenses/bsd-license.php The BSD License
+ *
+ */
+
+if ( ! class_exists('Wizin_Core_App') ) {
+	require dirname( dirname(__FILE__) ) . '/Wizin.class.php';
+	/**
+	 * Wizin framework core controller class
+	 *
+	 */
+	class Wizin_Core_App extends Wizin_StdClass
+	{
+		/**
+		 * Return 'Wizin_Core_App' singleton object
+		 */
+		public function &getSingleton()
+		{
+			static $instance;
+			if ( ! isset($instance) ) {
+				$instance = new Wizin_Core_App();
+			}
+			return $instance;
+		}
+
+		/**
+		 * System total control function.
+		 *
+		 */
+		public function execute()
+		{
+			// set member objects
+			$this->_setObjects();
+			// check client
+			$lookup = ( defined('WIZ_SYS_LOOKUP') ) ? WIZ_SYS_LOOKUP : false;
+			$this->_checkClient( $lookup );
+			/* TODO : refine MVC design
+			 *      : write input filter in request class
+			 */
+			// input filter
+            //$this->_addInputFilter();
+			//$this->_executeInputFilter();
+
+			// execute common action
+			$this->_executeCommon();
+			// execute controller process
+			$this->oController->execute();
+		}
+
+		/**
+		 * set member objects
+		 *
+		 */
+		protected function _setObjects()
+		{
+			// set extra headers
+			$this->sExtraHeader = '';
+			// set user class object
+			$user =& Wizin_User::getSingleton();
+			$this->oUser = new Wizin_Ref( $user );
+			// set filter class object
+			$filter =& Wizin_Filter::getSingleton();
+			$this->oFilter = new Wizin_Ref( $filter );
+		}
+
+		/**
+		 * check user by client
+		 *
+		 */
+		protected function _checkClient( $lookup = false )
+		{
+			// check client
+			$this->oUser->checkClient( $lookup );
+		}
+
+        /**
+         * Add input filter functions
+         */
+        protected function _addInputFilter()
+        {
+            $params = array( $this->oUser->sEncoding );
+            $this->oFilter->addInputFilter( array( $this->oFilter, 'filterInputEncoding' ), $params );
+        }
+
+		/**
+		 * Execute input filter functions
+		 */
+		protected function _executeInputFilter()
+		{
+			$this->oFilter->executeInputFilter();
+		}
+
+		/**
+		 * Execute common action
+		 */
+		protected function _executeCommon()
+		{
+			//
+			// analyze action from 'PATH_INFO
+			//
+			$pathInfo = getenv( 'PATH_INFO' );
+			if ( ! empty($pathInfo) ) {
+				if ( strpos($pathInfo, '.') !== false ) {
+					$pathInfoArray = explode( '.', $pathInfo );
+					array_pop( $pathInfoArray );
+					$pathInfo = implode( '.', $pathInfoArray );
+				}
+			} else {
+				$pathInfo = '/';
+			}
+			$this->sPathInfo = $pathInfo;
+			$this->sPathTranslated = $this->_getPathTranslated( $pathInfo );
+			$this->_dispatch( $this->sPathTranslated );
+		}
+
+		/**
+		 * return translated path
+		 *
+		 * @param string $pathInfo
+		 * @return string
+		 */
+		protected function _getPathTranslated( $pathInfo = '/' )
+		{
+			$pathTranslated = $pathInfo;
+			// if last character is '/', add default action
+			if ( substr($pathTranslated, -1, 1) === '/' ) {
+				$pathTranslated .= 'index';
+			}
+			$pathTranslated = dirname( $pathTranslated ) . '/' . ucfirst( basename($pathTranslated) );
+			if ( substr($pathTranslated, 0, 1) === '/' ) {
+				$pathTranslated = substr( $pathTranslated, 1 );
+			}
+			return $pathTranslated;
+		}
+
+		protected function _dispatch( $pathTranslated )
+		{
+			$pathArray = explode( '/', $pathTranslated );
+			$pathArray = array_map( 'ucfirst', $pathArray );
+			// controller
+			$controllerClass = WIZIN_DEFAULT_CONTROLLER;
+			$controllerPath = WIZ_SITE_ROOT . '/controllers/' . $pathTranslated . '.php';
+			if ( file_exists($controllerPath) ) {
+				require $controllerPath;
+				$class = WIZ_SYS_PREFIX . '_' . implode( '_', $pathArray ) . '_Controller';
+				if ( class_exists($class) ) {
+					$controllerClass = $class;
+				}
+			}
+			$controller = new $controllerClass();
+			$this->oController = new Wizin_Ref( $controller );
+			// view
+			$viewClass = WIZIN_DEFAULT_VIEW;
+			$viewPath = WIZ_SITE_ROOT . '/views/' . $pathTranslated . '.php';
+			if ( file_exists($viewPath) ) {
+				require $viewPath;
+				$class = WIZ_SYS_PREFIX . '_' . implode( '_', $pathArray ) . '_View';
+				if ( class_exists($class) ) {
+					$viewClass = $class;
+				}
+			}
+			$view = new $viewClass();
+			$this->oController->setView( $view );
+		}
+
+		/**
+		 * execute page action
+		 */
+		/*
+		protected function _executePage()
+		{
+			$isAccessible = $this->_isAccessible();
+			$renderer = get_class( $this->oSiteRenderer );
+			$pageRenderer = new $renderer();
+			// the user can access this page
+			if ( $isAccessible ) {
+				$templateName = $this->sPathTranslated . '.html';
+				$templateExists = $this->oSiteRenderer->template_exists( $templateName );
+				$scriptFile = WIZ_SITE_ROOT . '/controller/' . $this->sPathTranslated . '.php';
+				$scriptExists = ( file_exists($scriptFile) && is_readable($scriptFile) ) ? true : false;
+				if ( $templateExists || $scriptExists ) {
+					// execute page process
+					if ( $scriptExists ) {
+						require $scriptFile;
+						$pathArray = explode( '/', $this->sPathTranslated );
+						$pathArray = array_map( 'ucfirst', $pathArray );
+						$class = WIZ_SYS_PREFIX . '_' . implode( '_', $pathArray ) . '_Controller';
+						if ( class_exists($class) ) {
+							$_templateName = $templateName;
+							$controller = new $class( $pageRenderer, $templateName );
+							if ( $_templateName !== $templateName ) {
+								$templateExists = $this->oSiteRenderer->template_exists( $templateName );
+							}
+						}
+					}
+					// display template
+					if ( $templateExists ) {
+						$pageRenderer->display( $templateName );
+					}
+				} else {
+					// display error page
+					$pageRenderer->display( 'error.html' );
+				}
+			} else {
+				// display error page
+				$pageRenderer->display( 'error.html' );
+			}
+		}
+		*/
+
+	}
+}
--- wizin/src/core/Controller.class.php
+++ wizin/src/core/Controller.class.php
@@ -4,225 +4,46 @@
  *
  * PHP Version 5.2 or Upper version
  *
- * @package  giftbox.in
+ * @license http://www.opensource.org/licenses/bsd-license.php The BSD License
+ * @link http://www.gusagi.com/
  * @author  Makoto Hashiguchi a.k.a. gusagi<gusagi@gusagi.com>
  * @copyright 2008 Makoto Hashiguchi
- * @license http://www.opensource.org/licenses/bsd-license.php The BSD License
  *
  */
 
 if ( ! class_exists('Wizin_Core_Controller') ) {
-    require dirname( dirname(__FILE__) ) . '/Wizin.class.php';
-    /**
-     * Wizin framework core controller class
-     *
-     */
-    class Wizin_Core_Controller extends Wizin_StdClass
-    {
-        /**
-         * Return 'Wizin_Core_Controller' singleton object
-         */
-        public function &getSingleton()
-        {
-            static $instance;
-            if ( ! isset($instance) ) {
-                $instance = new Wizin_Core_Controller();
-            }
-            return $instance;
-        }
+	require dirname( dirname(__FILE__) ) . '/Wizin.class.php';
+	/**
+	 * Wizin framework core controller class
+	 *
+	 */
+	class Wizin_Core_Controller extends Wizin_StdClass
+	{
+		public function __construct()
+		{
+		}
 
-        /**
-         * System total control function.
-         *
-         */
-        public function execute()
-        {
-            // set member variables
-            $this->_setVariables();
-            // input filter
-            $this->_addInputFilter();
-            $this->_executeInputFilter();
-            // execute common action
-            $this->_executeCommon();
-            // execute page action
-            $this->_executePage();
-            // output filter
-            $this->_addOutputFilter();
-            $contents = $this->_executeOutputFilter();
-            // display
-            $this->_display( $contents );
-        }
+		public function setView( & $viewObject )
+		{
+			$this->_oView = new Wizin_Ref( $viewObject );
+		}
 
-        /**
-         * set member variables
-         *
-         */
-        protected function _setVariables()
-        {
-            // set user class object
-            $user =& Wizin_User::getSingleton();
-            $this->user = $user;
-            // check client
-            $this->_checkUser();
-            // set filter class object
-            $filter =& Wizin_Filter::getSingleton();
-            $this->filter = $filter;
-            // set renderer class object
-            $this->siteRenderer = new Wizin_Core_Renderer();
-        }
+		public function execute()
+		{
+			// execute main process
+			$this->_main();
+			// execute view
+			$this->_executeView();
+		}
 
-        /**
-         * check user by client
-         *
-         */
-        protected function _checkUser()
-        {
-            // check client
-            $this->user->checkClient();
-        }
+		protected function _main()
+		{
+		}
 
-        /**
-         * Add input filter functions
-         */
-        protected function _addInputFilter()
-        {
-            $params = array( $this->user->sEncoding );
-            $this->filter->addInputFilter( array( $this->filter, 'filterInputEncoding' ), $params );
-            $this->filter->executeInputFilter();
-        }
+		protected function _executeView()
+		{
+			$this->_oView->execute();
+		}
 
-        /**
-         * Execute input filter functions
-         */
-        protected function _executeInputFilter()
-        {
-            $this->filter->executeInputFilter();
-        }
-
-        /**
-         * Add output filter functions
-         */
-        protected function _addOutputFilter()
-        {
-            $params = array( $this->user->sEncoding, $this->user->sCharset );
-            $this->filter->addOutputFilter( array($this->filter, 'filterOutputEncoding'), $params );
-        }
-
-        /**
-         * Execute output filter functions
-         */
-        protected function _executeOutputFilter()
-        {
-            $pageContents = ob_get_clean();
-            $this->siteRenderer->assign( 'siteUrl', 'http://' . getenv('SERVER_NAME') );
-            $this->siteRenderer->assign( 'siteTitle', 'Wizin initial template.' );
-            $this->siteRenderer->assign( 'doctype', $this->user->sDoctype );
-            $this->siteRenderer->assign( 'pageContents', $pageContents );
-            $contents = $this->siteRenderer->fetch(
-                'file:' . $this->siteRenderer->template_dir . 'site.html' );
-            $this->filter->executeOutputFilter( $contents );
-            return $contents;
-        }
-
-        /**
-         * Display contents
-         */
-        protected function _display( $contents )
-        {
-            if ( $this->user->bIsMobile ) {
-                $contentType = 'application/xhtml+xml';
-            } else {
-                $contentType = 'text/html';
-            }
-            header( 'Content-Type:' . $contentType . '; charset=' . $this->user->sCharset );
-            header( 'Content-Length: ' . strlen($contents) );
-            echo $contents;
-        }
-
-        /**
-         * Execute common action
-         */
-        protected function _executeCommon()
-        {
-            //
-            // analyze action from 'PATH_INFO
-            //
-            $pathInfo = getenv( 'PATH_INFO' );
-            if ( ! empty($pathInfo) ) {
-                if ( strpos($pathInfo, '.') !== false ) {
-                    $pathInfoArray = explode( '.', $pathInfo );
-                    array_pop( $pathInfoArray );
-                    $pathInfo = implode( '.', $pathInfoArray );
-                }
-            } else {
-                $pathInfo = '/';
-            }
-            $this->sPathInfo = $pathInfo;
-            $this->sPathTranslated = $this->_getPathTranslated( $pathInfo );
-        }
-
-        /**
-         * return translated path
-         *
-         * @param string $pathInfo
-         * @return stromg
-         */
-        protected function _getPathTranslated( $pathInfo = '/' )
-        {
-            $pathTranslated = $pathInfo;
-            // if last character is '/', add default action
-            if ( substr($pathTranslated, -1, 1) === '/' ) {
-                $pathTranslated .= 'index';
-            }
-            return $pathTranslated;
-        }
-
-        /**
-         * execute page action
-         */
-        protected function _executePage()
-        {
-            $isAccessible = $this->_isAccessible();
-            $pageRenderer = clone $this->siteRenderer;
-            // the user can access this page
-            if ( $isAccessible ) {
-                $template = $this->sPathTranslated . '.html';
-                $templateExists = $this->siteRenderer->template_exists( $template );
-                $script = WIZ_SITE_ROOT . '/pages' . $this->sPathTranslated . '.php';
-                $scriptExists = ( file_exists($script) && is_readable($script) ) ? true : false;
-                if ( $templateExists || $scriptExists ) {
-                    // execute page process
-                    if ( $scriptExists ) {
-                        require $script;
-                        $pathArray = explode( '/', $this->sPathTranslated );
-                        $pathArray = array_map( 'ucfirst', $pathArray );
-                        $class = WIZ_SYS_PREFIX . '_Page' . implode( '_', $pathArray );
-                        if ( class_exists($class) ) {
-                            $page = new $class( $pageRenderer );
-                        }
-                    }
-                    // display template
-                    if ( $templateExists ) {
-                        $pageRenderer->display( $template );
-                    }
-                } else {
-                    // display error page
-                    $pageRenderer->display( '/error.html' );
-                }
-            } else {
-                // display error page
-                $pageRenderer->display( '/error.html' );
-            }
-        }
-
-        /**
-         * is the user can access this page
-         *
-         */
-        protected function _isAccessible()
-        {
-            return true;
-        }
-
-    }
+	}
 }
