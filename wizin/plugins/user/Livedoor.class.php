e9ccd493332fd678afac0cd8fd4ba8d875247db2
gusagi <gusagi@gmail.com>
2009-05-27 13:17:54 +0000

    Added client plugin for 'Livedoor' and 'Mixi'.
    git-svn-id: file://localhost/home/repos/svn/wizin/trunk@333 fd31610b-4be5-dc11-a523-00018064d71f

---
+++ wizin/plugins/user/Livedoor.class.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ *
+ * PHP Versions 4
+ *
+ * @package  Wizin
+ * @author  Makoto Hashiguchi a.k.a. gusagi<gusagi@gusagi.com>
+ * @copyright 2008 Makoto Hashiguchi
+ * @license http://www.opensource.org/licenses/bsd-license.php The BSD License
+ *
+ */
+
+if (! class_exists('Wizin_Plugin_User_Livedoor')) {
+    require dirname(__FILE__) .'/Mobile.class.php';
+    class Wizin_Plugin_User_Livedoor extends Wizin_Plugin_User_Mobile
+    {
+        function __construct()
+        {
+            $this->_require();
+            $this->_setup();
+        }
+
+        function _require()
+        {
+        }
+
+        function _setup()
+        {
+            $this->_checkMobile();
+            parent::_setup();
+        }
+
+        function _checkMobile()
+        {
+            $agent = getenv('HTTP_USER_AGENT');
+            $user =& Wizin_User::getSingleton();
+            $pattern = '/([a-zA-Z_0-9\/.;() -]+)(LD_mobile_bot)([a-zA-Z_0-9\/.;() -]+)/i';
+            if (preg_match($pattern, $agent, $matches)) {
+                $user->bIsMobile = true;
+            } else {
+                $user->bIsMobile = false;
+            }
+        }
+    }
+}
