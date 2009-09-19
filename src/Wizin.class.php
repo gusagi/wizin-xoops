76a858be5e9e5e191479e3cfd55284a941f44c50
gusagi <gusagi@gmail.com>
2009-09-20 02:21:08 +0900

    Deleted constant 'WIZIN_SALT'.

--- src/Wizin.class.php
+++ src/Wizin.class.php
@@ -36,12 +36,6 @@ if (! defined('WIZIN_ROOT_PATH')) {
             function __construct()
             {
                 //
-                // WIZIN_SALT
-                if (! defined('WIZIN_SALT')) {
-                    define('WIZIN_SALT', substr(md5(__FILE__), 0, 8));
-                }
-
-                //
                 // REQUEST_URI
                 $requestUri = getenv('REQUEST_URI');
                 if (empty($requestUri)) {
@@ -110,6 +104,7 @@ if (! defined('WIZIN_ROOT_PATH')) {
                 }
                 return $instance;
             }
+
         }
     }
 }
