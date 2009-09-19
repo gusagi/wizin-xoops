44b57ee4193dbb20ea97f096dcff77550a5aec13
gusagi <gusagi@gmail.com>
2009-09-20 01:03:21 +0900

    Added 'PEAR::Crypt_Blowfish'.

---
+++ src/Wizin_Crypt.class.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Wizin framework PEAR::Crypt_Blowfish wrapper class
+ *
+ * PHP Versions 4
+ *
+ * @package  Wizin
+ * @author  Makoto Hashiguchi a.k.a. gusagi<gusagi@gusagi.com>
+ * @copyright 2008 Makoto Hashiguchi
+ * @license http://www.opensource.org/licenses/bsd-license.php The BSD License
+ *
+ */
+
+if (! class_exists('Wizin_Crypt')) {
+    require dirname(__FILE__) . '/Wizin.class.php';
+    if (! class_exists('Crypt_Blowfish')) {
+        require WIZIN_ROOT_PATH . '/lib/PEAR/Crypt/Blowfish.php';
+    }
+
+    /**
+     * PEAR::Crypt_Blowfish wrapper class
+     *
+     * @access public
+     *
+     */
+    class Wizin_Crypt
+    {
+        function & getBlowfish($key = '')
+        {
+            static $blowfish;
+            if (! isset($blowfish)) {
+                if ($key === '') {
+                    $key = Wizin_Util::salt();
+                }
+                $iv = substr(md5(WIZIN_SALT ."\t" .__FILE__, 1), 0, 8);
+                $blowfish =& Crypt_Blowfish::factory('cbc', $key, $iv);
+            }
+            return $blowfish;
+        }
+    }
+}
